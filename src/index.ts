5d6893d409224638762b7755a10c0ab5a179275e
MarkChai <87580250+HydroGest@users.noreply.github.com>
2025-01-23 21:21:44 +0800

    fix: 使用更高级的 ping 方法

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Context, Schema } from 'koishi';
-const { MinecraftServerListPing } = require("minecraft-status");
+
+const mcpinger = require('minecraft-server-ping')
+ 
 
 export const name = 'custom-mc-info';
 
@@ -8,33 +10,6 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
-  // 获取Minecraft服务器信息
-  async function getMinecraftServerInfo(address: string, port: number): Promise<any> {
-      MinecraftServerListPing.ping(4, address, port, 3000)
-      .then(response => {
-          return response;
-      })
-      .catch(error => {
-          // Error
-      });
-  }
-
-  // 此函数用于测试单个域名的延迟
-  async function testLatency(domain: string): Promise<number> {
-      const startTime = performance.now(); // 记录开始时间
-      try {
-          // 发起一个HEAD请求来测试延迟
-          const response = await fetch(`https://${domain}`, { method: 'HEAD' });
-          if (!response.ok) {
-              throw new Error(`Request to ${domain} failed with status ${response.status}`);
-          }
-      } catch (error) {
-          // 如果请求失败，返回一个非常大的延迟值
-          return Infinity;
-      }
-      const endTime = performance.now(); // 记录结束时间
-      return endTime - startTime; // 计算并返回延迟
-  }
 
   // 此函数用于找出延迟最低的服务器
   async function findLowestLatencyServer(domains: string[]): Promise<{ domain: string; latency: number } | null> {
@@ -47,9 +22,11 @@ export function apply(ctx: Context) {
 
       // 遍历所有域名，测试它们的延迟
       for (const domain of domains) {
-          const latency = await testLatency(domain);
-          if (latency < lowestLatency) {
-              lowestLatency = latency;
+          const [address, portStr] = domain.split(':');
+          
+          const data = await mcpinger.ping(() => {hostname: address, port: parseInt(portStr)});
+          if (data.ping < lowestLatency) {
+              lowestLatency = data.ping;
               lowestLatencyDomain = domain;
           }
       }
@@ -66,16 +43,16 @@ export function apply(ctx: Context) {
           let returnMsg = "";
           const [address, portStr] = 'frp-nut.top:50804'.split(':');
           const port = parseInt(portStr, 10);
-          const serverInfo = await getMinecraftServerInfo(address, port);
+          const serverInfo = await mcpinger.ping(() => {hostname: address, port: parseInt(portStr) });
           if (serverInfo) {
               returnMsg += `服务器当前人数: ${serverInfo.players.online} \n`;
           }
 
           const domains = [
-              'frp-bag.top',
-              'frp-gym.top',
-              'frp-fee.top',
-              'frp-nut.top'
+              'frp-bag.top:15920',
+              'frp-gym.top:50516',
+              'frp-fee.top:50508',
+              'frp-nut.top:50584'
           ];
 
           const result = await findLowestLatencyServer(domains);
